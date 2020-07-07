e38f0d63cc386a69e3dfbb970ca616d55d12525e
Omri Galai <omri@galai.biz>
2020-07-07 22:36:46 +0300

    Modified complete.R Rudimentary version of corr.R

--- complete.R
+++ complete.R
@@ -1,7 +1,7 @@
 
 complete <- function(directory, id = 1:332) {
   require(dplyr)
-  path <- paste(".",directory,paste(stringr::str_pad(id,width=3,pad='0'),".csv",sep=""),sep="/")
+  path <- paste(".",directory,paste0(stringr::str_pad(id,width=3,pad='0'),".csv"),sep="/")
   mydata <- ldply(path,read.csv)
   # head(mydata)
   mydata$ID <- as.factor(mydata$ID)
@@ -9,7 +9,7 @@ complete <- function(directory, id = 1:332) {
   mydata %>%
   mutate(comp=complete.cases(mydata)) %>%
   group_by(ID) %>%
-  summarise("Nobs"=sum(comp))
+  summarise("nobs"=sum(comp))
   
   
   
---
+++ corr.R
@@ -0,0 +1,18 @@
+corr <- function(directory, threshold = 0) {
+  # directory contains 332 csv files containing pollutant data
+  # corr returns vector of correlations between nitrate and sulfate for datasets above a threshold of complete observations
+  path <- paste(".",directory,paste(stringr::str_pad(1:332,width=3,pad='0'),".csv",sep=""),sep="/")
+  mydata <-lapply(path,read.csv)
+  comp <- complete.cases(data.matrix(unlist(mydata)))
+  # comp1 <- comp[(sum(comp)>threshold)]
+  comp <- numeric()
+  for(s in mydata) {
+    comp[s$ID] <- sum(complete.cases(data.matrix(s)))
+  }
+  corel <- numeric()
+  for(s in mydata) {
+    corel[s$ID] <- cor(data.matrix(s[-1,]$nitrate),data.matrix(s[-1,]$sulfate),use="pairwise.complete.obs",method="pearson")
+  }
+  corel[(comp>threshold)]
+
+}
